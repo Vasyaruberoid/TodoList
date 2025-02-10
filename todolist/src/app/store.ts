6fc6a7d91cafad9e36d64fb649f352e95229fcbc
Vasyaruberoid <84591840+Vasyaruberoid@users.noreply.github.com>
2025-02-10 23:02:07 +0300

    refactor App in Redux,Axios

---
+++ todolist/src/app/store.ts
@@ -0,0 +1,25 @@
+import { combineReducers } from "redux"
+import { authReducer, authSlice } from "../features/auth/model/authSlice"
+import { tasksReducer, tasksSlice } from "../features/todolists/model/tasksSlice"
+import { todolistsReducer, todolistsSlice } from "../features/todolists/model/todolistsSlice"
+import { appReducer, appSlice } from "./appSlice"
+import { configureStore } from "@reduxjs/toolkit"
+
+export const store = configureStore({
+  reducer: {
+    [tasksSlice.name]: tasksReducer,
+    [todolistsSlice.name]: todolistsReducer,
+    [appSlice.name]: appReducer,
+    [authSlice.name]: authReducer,
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+
+// export type AppDispatch = typeof store.dispatch
+
+// Создаем тип диспатча который принимает как AC так и TC
+export type AppDispatch = typeof store.dispatch
+
+// @ts-ignore
+window.store = store
